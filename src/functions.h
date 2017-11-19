6f48622a0a1a753f5c12a8e50a3d01639036ac56
SlinkyMation <jmmorris2@mail.bradley.edu>
2017-11-18 21:03:41 -0600

    will be good after code cleanup

--- src/functions.h
+++ src/functions.h
@@ -1,8 +1,10 @@
-
-//function declarations
+/*
+    Project BUScore by Jason and Kla
+    This project file is for the "HoverBoard" (Particle Photon wifi module)
+    It declares all functions
+                                                                                    */
 
 void gotScores(const char *, const char *);
 void pregame();
+void livegame(char *data);
 void postgame(char *);
-void quarterGoing();
-void intermission();
--- src/functions.ino
+++ src/functions.ino
@@ -5,6 +5,36 @@ String data_byte;
 
 void gotScores(const char *event, const char *data) {
 
+  data_byte = strtok(strdup(data), ",");
+  String isUnplayed = strtok(NULL, ",");
+  String isinProgress = strtok(NULL, ",");
+  String isCompleted = strtok(NULL, ",");
+  String home_score = strtok(NULL, ",");
+  String away_score = strtok(NULL, ",");
+  String quarter = strtok(NULL, ",");
+  String intermission = strtok(NULL, ",");
+  String seconds_remaining = strtok(NULL, ",");
+
+  Serial.println();
+  Serial.print("data = ");
+  Serial.println(data);
+  Serial.print("isUnplayed = ");
+  Serial.println(isUnplayed);
+  Serial.print("isinProgress = ");
+  Serial.println(isinProgress);
+  Serial.print("isCompleted = ");
+  Serial.println(isCompleted);
+  Serial.print("home_score = ");
+  Serial.println(home_score);
+  Serial.print("away_score = ");
+  Serial.println(away_score);
+  Serial.print("quarter = ");
+  Serial.println(quarter);
+  Serial.print("intermission = ");
+  Serial.println(intermission);
+  Serial.print("seconds_remaining = ");
+  Serial.println(seconds_remaining);
+
     static String data_temp = "";
 
     Serial.println();
@@ -12,16 +42,10 @@ void gotScores(const char *event, const char *data) {
     Serial.println(data);
 
     data_byte = strtok(strdup(data), ",");
-    Serial.println("datatemp = " + data_temp);
 
-    // if(data == data_temp || data_byte != "data") {
-    if (strcmp(data_byte, "data") != 0){
-      Serial.println("nothing happened cuz data_byte != 'data'");
+    if ( strcmp(data_byte, "data") != 0 || strcmp(data, data_temp) == 0) {
+        Serial.println("no update");
     }
-
-    // else if (strcmp(data, data_temp) == 0) {
-    //   Serial.println("nothing happened cuz data == data_temp");
-    // }
 
     else {
 
@@ -29,23 +53,15 @@ void gotScores(const char *event, const char *data) {
   		String isinProgress = strtok(NULL, ",");
   		String isCompleted = strtok(NULL, ",");
 
-      Serial.print("isUnplayed = ");
-      Serial.println(isUnplayed);
-      Serial.print("isinProgress = ");
-      Serial.println(isinProgress);
-      Serial.print("isCompleted = ");
-      Serial.println(isCompleted);
-      Serial.println();
-
       if (strcmp(isUnplayed, "true") == 0) {
         pregame();
       }
 
       else if (strcmp(isinProgress, "true") == 0) {
+        livegame(strdup(data));
       }
 
       else if (strcmp(isCompleted, "true") == 0) {
-        Serial.println("it's completed");
         postgame(strdup(data));
       }
     }
@@ -61,12 +77,39 @@ void pregame() {
 
 }
 
+void livegame(char *data) {
 
-void postgame(char *data){
+  String nothing = strtok(strdup(data), ",");
+  nothing = strtok(NULL, ",");
+  nothing = strtok(NULL, ",");
+  nothing = strtok(NULL, ",");
+  String home_score = strtok(NULL, ",");
+  String away_score = strtok(NULL, ",");
+  String period = strtok(NULL, ",");
+  String seconds_remaining = strtok(NULL, ",");
+
+  if (strcmp(seconds_remaining, "") == 0) {
+    Serial.println("data," + home_score + "," + away_score + "," + period + ",0,0");
+  }
+
+  else {
+
+    int int_seconds = atoi( seconds_remaining );
+    int minutes = int_seconds / 60;
+    int seconds = int_seconds % 60;
+
+    Serial.println("data," + home_score + "," + away_score + "," + period + "," + minutes +
+      "," + seconds);
+  }
+
+}
+
+void postgame(char *data) {
 
 		String nothing = strtok(strdup(data), ",");
 		nothing = strtok(NULL, ",");
 		nothing = strtok(NULL, ",");
+    nothing = strtok(NULL, ",");
 		String home_score = strtok(NULL, ",");
 		String away_score = strtok(NULL, ",");
 
