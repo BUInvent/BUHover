f6c461b8ca36de4557d723ef33ba3a0b7c73f289
Jason Morris <jmmorris2@mail.bradley.edu>
2018-02-06 22:42:34 -0600

    Cleanup code
    Add comments
    Switch functions.ino to functions.cpp
    take out livegame functions
    print the game as finished if just is unplayed and isinprogress are both false
    	instead of making sure isCompleted is false

--- src/BUHover.ino
+++ src/BUHover.ino
@@ -1,55 +1,53 @@
   /*
-      Project BUScore by Jason and Kla
+      Project BUScore by Jason
       This project file is for the "HoverBoard" (Particle Photon wifi module)
       It is the main function that will  grab data from the webhook, parse it, and send it to the "BattleShip"
                                                                                       */
 
 #include "functions.h"
 
-char buf[256];
-char date[10];
+// declare global variables
+int team_league(String team_league);
+char date_team[256];
 String league;
-String team;
 
-int teamLeague(String teamLeague);
 
 void setup() {
 
-    Particle.function("teamLeague", teamLeague);
-    Time.zone(-4);
-    // teamLeague("NBA CHI");
-
+  // expose the team_league function so that the app can call it
+  // and set the time zone for when we get the date
+  Particle.function("team_league", team_league);
+  Time.zone(-7);
 
 }
 
 
 void loop() {
-    static unsigned long nextTrigger = 10 * 1000;
 
     // pull the webhook every 10 seconds
-    if (nextTrigger < millis()) {
-        nextTrigger = millis() + 10*1000;
-        Particle.publish( league + "Scores", buf, PRIVATE );
+    Particle.publish( league + "Scores", date_team, PRIVATE );
+    delay(10000);
 
-    }
 }
 
-// this function automagically gets called upon a matching POST request
-int teamLeague(String teamLeague)
-{
-  // look for the matching argument "coffee" <-- max of 64 characters long
-  if(teamLeague != "")
-  {
 
-    league = strtok( strdup(teamLeague), " " );
-    team = strtok( NULL, "," );
+// the input will be a string in the format: "league team"
+// This function will grab the league and team by parsing the string and will
+// subscribe to the leagues specified webhook
+// it will send the date and team to the webhook so it knows what URL path to use
+// for the specified game
+int team_league(String team_league) {
 
-    Particle.subscribe("hook-response/" + league + "Scores", gotScores, MY_DEVICES);
-    sprintf(date,  "%d%02d%02d", Time.year(), Time.month(), Time.day());
-    snprintf(buf, sizeof(buf), "{\"date\":\"%s\",\"team\":\"%s\"}", date, team.c_str());
+  char date[10];
+  String team;
 
-    return 1;
+  league = strtok( strdup(team_league), " " );
+  team = strtok( NULL, "," );
 
-  }
-  else return -1;
+  Particle.subscribe("hook-response/" + league + "Scores", got_scores, MY_DEVICES);
+  sprintf(date,  "%d%02d%02d", Time.year(), Time.month(), Time.day());
+  snprintf(date_team, sizeof(date_team), "{\"date\":\"%s\",\"team\":\"%s\"}", date, team.c_str());
+
+  return 1;
+
 }
--- src/functions.h
+++ src/functions.h
@@ -1,8 +1,7 @@
 /*
-    Project BUScore by Jason and Kla
+    Project BUScore by Jason
     This project file is for the "HoverBoard" (Particle Photon wifi module)
     It declares all functions
                                                                                     */
 
-void gotScores(const char *, const char *);
-String livegame(char *data);
+void got_scores(const char *, const char *);
